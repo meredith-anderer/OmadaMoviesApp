7912b0b8af739cac32bedc931362f526f16661d1
Meredith Anderer <mra85@cornell.edu>
2025-04-21 10:56:11 -0500

    Initial Commit

---
+++ OmadaMoviesApp/OmadaMoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmadaMoviesAppApp.swift
+//  OmadaMoviesApp
+//
+//  Created by Meredith Anderer on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct OmadaMoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OmadaMoviesAppTests/OmadaMoviesAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  OmadaMoviesAppTests.swift
+//  OmadaMoviesAppTests
+//
+//  Created by Meredith Anderer on 4/21/25.
+//
+
+import Testing
+
+struct OmadaMoviesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
